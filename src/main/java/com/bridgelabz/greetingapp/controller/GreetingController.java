85995d2a4e7e4a35c5c2ac37529e2eea0f2eebfd
rahulgidde.com <rahulgidde1212@gmail.com>
2020-04-30 13:00:35 +0530

    Added:Using Greeting Controller Return Json For Get Method

--- src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,21 +1,24 @@
 package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.dto.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping("/hello")
+@RequestMapping("/greetings")
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping(value = "/query", method = RequestMethod.GET)
+    public Greeting getGreeting(@RequestParam(value = "name", defaultValue = "world") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
